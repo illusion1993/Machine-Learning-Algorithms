466f33562c59798ff7f50c3791fc35a3fcec0ded
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2016-05-26 03:15:22 +0530

    Dataset class changes, added simple sum function dataset file

---
+++ 1_dataset_test.cpp
@@ -0,0 +1,9 @@
+#include "lib/linear_regression.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	Dataset d(2, 3);
+	return 0;
+}
--- lib/linear_regression.h
+++ lib/linear_regression.h
@@ -2,16 +2,35 @@
 using namespace std;
 
 class Dataset {
-	long long number_of_samples_, number_of_features_;
+	int size_, depth_;
 	vector< vector<double> > inputs_;
 	vector<double> outputs_;
+
+	void addBiasInput();
 public:
 	Dataset();
-	Dataset(long long number_of_samples, long long number_of_features);
+	Dataset(int, int);
+	Dataset(std::ifstream&, int, int);
+
+	void readDatasetFile(std::ifstream&, int, int);
 };
 
 Dataset::Dataset() {}
-Dataset::Dataset(long long number_of_samples_, long long number_of_features_) {
-	number_of_samples_ = number_of_samples;
-	number_of_features_ = number_of_features_;
+Dataset::Dataset(int size, int depth) : size_(size), depth_(depth) {
+	inputs_.resize(size, vector<double>(depth));
+	outputs_.resize(size);
+}
+Dataset::Dataset(std::ifstream &fin, int size, int depth) : size_(size), depth_(depth) {
+	inputs_.resize(size, vector<double>(depth));
+	outputs_.resize(size);
+	readDatasetFile(fin, size, depth);
+}
+void Dataset::readDatasetFile(std::ifstream &fin, int size, int depth) : size_(size), depth_(depth) {
+	// Clear previous data, this may be called to load another dataset file
+
+	// After reading and storing dataset, add a bias input (1.0)
+	addBiasInput();
+}
+void Dataset::addBiasInput() {
+	// Do not proceed if already added a bias input
 }
