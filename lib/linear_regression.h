35225e8efec3ef1642520aff3987ecd72e0e6a54
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2016-05-26 02:29:18 +0530

    Dataset class

--- lib/linear_regression.h
+++ lib/linear_regression.h
@@ -1,2 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Dataset {
+	long long number_of_samples_, number_of_features_;
+	vector< vector<double> > inputs_;
+	vector<double> outputs_;
+public:
+	Dataset();
+	Dataset(long long number_of_samples, long long number_of_features);
+};
+
+Dataset::Dataset() {}
+Dataset::Dataset(long long number_of_samples_, long long number_of_features_) {
+	number_of_samples_ = number_of_samples;
+	number_of_features_ = number_of_features_;
+}
