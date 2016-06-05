397deff9279cb76400c130cc844cb5bd59b2d1ca
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2016-06-05 10:13:23 +0530

    Added test function in linear regressor

--- 2_linear_regressor_test.cpp
+++ 2_linear_regressor_test.cpp
@@ -7,12 +7,44 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	Dataset d, t, e, u;
+	LinearRegressor lr, tr;
+
+
 	ifstream fin ("Datasets/sum_function_size_5_depth_2.txt");
-	Dataset d;
 	d.readDatasetFile(fin, 5, 2, true);
-	// d.normalize();
-	LinearRegressor lr;
-	lr.train(&d, 0.01, 0.0001);
+	
+	ifstream tin ("Datasets/sum_function_size_5_depth_2_test.txt");
+	t.readDatasetFile(tin, 5, 2, false);
+
+	/*
+	Below dataset contains a linear equation in 4 variables (hyperplane_equation_size_50_depth_3.txt)
+	For a linear equation of n variables, minimum of (n + 1) samples are required to find a solution
+	So, for a linear equation of 1 variable like mx + c = y(eqn of a line), minimum 2 samples are required
+	Which is correct because to find a line, you need at least 2 points that lie on it
+	To find a plane, you need at least 3 points and for a hyperplane, 4 points and so on
+	*/
+	ifstream gin ("Datasets/hyperplane_equation_size_50_depth_3.txt");
+	e.readDatasetFile(gin, 50, 3, true);
+	
+	ifstream uin ("Datasets/hyperplane_equation_size_50_depth_3_test.txt");
+	u.readDatasetFile(uin, 50, 3, false);
+
+	
+
+	// Training and testing on simple sum function
+	lr.train(&d, 0.02, 0.0000001);
 	lr.printResults();
+	cout << "\n\nNOW TESTING:\n\n";
+	lr.test(&t);
+
+	cout << "\n\nNEW PROBLEM---------------------\n";
+
+	tr.train(&e, 0.005, 0.0000001);
+	tr.printResults();
+	cout << "\n\nNOW TESTING:\n\n";
+	tr.test(&u);
+	
+
 	return 0;
 }
--- lib/linear_regressor.h
+++ lib/linear_regressor.h
@@ -4,25 +4,27 @@ using namespace std;
 
 class LinearRegressor {
 	// A regressor object can be trained by only one training dataset, due to normalization issues
-	Dataset * _training_dataset;
+	Dataset * _training_dataset, * _testing_dataset;
 	vector<double> _parameters;
 	
-	double trainingHypothesisValue(int sample_number);
+	double hypothesisValue(int sample_number, bool training = true);
 	void gradientDescent(double learning_rate, double allowed_rms_error);
 
 public:
 	void train(Dataset * training_dataset, double learning_rate, double allowed_rms_error);
+	void test(Dataset * testing_dataset);
 	void printResults();
 };
 
-double LinearRegressor::trainingHypothesisValue(int sample_number) {
+double LinearRegressor::hypothesisValue(int sample_number, bool training /*=true*/) {
 	/*
 	Returns the value of hypothesis for a particular training dataset sample
 	based on current values of all parameters
 	*/
 	double hypothesis = 0.0;
-	for (int feature_number = 0; feature_number <= _training_dataset -> depth(); feature_number++) {
-		hypothesis += (_parameters[feature_number] * _training_dataset -> getInput(sample_number, feature_number));
+	Dataset * dataset_used = (training) ? _training_dataset : _testing_dataset;
+	for (int feature_number = 0; feature_number <= dataset_used -> depth(); feature_number++) {
+		hypothesis += (_parameters[feature_number] * dataset_used -> getInput(sample_number, feature_number));
 	}
 	return hypothesis;
 }
@@ -37,11 +39,11 @@ void LinearRegressor::gradientDescent(double learning_rate, double allowed_rms_e
 		// Calculating all hypothesis errors for parameters updated in last iteration
 		// Also calculating the RMS error value after last iteration
 		for (int sample_number = 0; sample_number < _training_dataset -> size(); sample_number++) {
-			hypothesis_error[sample_number] = trainingHypothesisValue(sample_number) - _training_dataset -> getOutput(sample_number);
+			hypothesis_error[sample_number] = hypothesisValue(sample_number) - _training_dataset -> getOutput(sample_number);
 			current_rms_error += (hypothesis_error[sample_number] * hypothesis_error[sample_number]);
 		}
 		current_rms_error = sqrt(current_rms_error / _training_dataset -> size());
-		cout << "RMS error is: " << current_rms_error << "\n";
+		// cout << "RMS error is: " << current_rms_error << "\n";
 
 		// If the RMS error has actually increased due to the last iteration, halt the process
 		if (current_rms_error > previous_rms_error) {
@@ -72,6 +74,13 @@ void LinearRegressor::train(Dataset * training_dataset, double learning_rate, do
 	gradientDescent(learning_rate, allowed_rms_error);
 }
 
+void LinearRegressor::test(Dataset * testing_dataset) {
+	_testing_dataset = testing_dataset;
+	for (int sample_number = 0; sample_number < _testing_dataset -> size(); sample_number++) {
+		cout << "Testing result: " << hypothesisValue(sample_number, false) << "\n";
+	}
+}
+
 void LinearRegressor::printResults() {
 	cout << "Final parameters:\n";
 	for (int i = 0; i < _training_dataset -> depth() + 1; i++) cout << _parameters[i] << " ";
@@ -80,6 +89,6 @@ void LinearRegressor::printResults() {
 		for (int j = 0; j < _training_dataset -> depth() + 1; j++) {
 			cout << _training_dataset -> getInput(i, j) << " ";
 		}
-		cout << ": " << _training_dataset -> getOutput(i) << ", h(X) = " << trainingHypothesisValue(i) << "\n";
+		cout << ": " << _training_dataset -> getOutput(i) << ", h(X) = " << hypothesisValue(i) << "\n";
 	}
 }
