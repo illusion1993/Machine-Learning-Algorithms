93bf75f830e9708769e10c78b2ae07d4c8082bf3
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2016-06-05 07:19:38 +0530

    Modified gradient descent algorithm to detect learning rates that are too high

--- lib/linear_regressor.h
+++ lib/linear_regressor.h
@@ -20,16 +20,33 @@ class LinearRegressor {
 	}
 	
 	void gradientDescent(double learning_rate, double allowed_rms_error) {
-		double rms_error = allowed_rms_error + 1;
+		double previous_rms_error = DBL_MAX, current_rms_error = DBL_MAX;
 		vector<double> hypothesis_error(training_dataset_ -> size());
 
-		while (rms_error > allowed_rms_error) {
-			rms_error = 0;
+		while (previous_rms_error > allowed_rms_error) {
+			current_rms_error = 0;
+
+			// Calculating all hypothesis errors for parameters updated in last iteration
+			// Also calculating the RMS error value after last iteration
 			for (int sample_number = 0; sample_number < training_dataset_ -> size(); sample_number++) {
 				hypothesis_error[sample_number] = trainingHypothesisValue(sample_number) - training_dataset_ -> getOutput(sample_number);
-				rms_error += (hypothesis_error[sample_number] * hypothesis_error[sample_number]);
+				current_rms_error += (hypothesis_error[sample_number] * hypothesis_error[sample_number]);
 			}
-			rms_error = sqrt(rms_error / training_dataset_ -> size());
+			current_rms_error = sqrt(current_rms_error / training_dataset_ -> size());
+			cout << "RMS error is: " << current_rms_error << "\n";
+
+			// If the RMS error has actually increased due to the last iteration, halt the process
+			if (current_rms_error > previous_rms_error) {
+				cout << "Learning rate chosen is too high! Halting Gradient Descent.\n";
+				break;
+			}
+			// Otherwise, current RMS error is now stored as previous RMS error
+			previous_rms_error = current_rms_error;
+
+			// Now updating the values of parameters
+			// For each parameter p, the partial derivative of the cost function wrt the parameter is found by
+			// Multiply the hypothesis error (h(theta) - Y) with the parameter's coefficient in that particular sample, for all samples
+			// Divide the sum of all those values by the number of samples. Parameter is decremented by learning rate * this value
 			for (int feature_number = 0; feature_number <= training_dataset_ -> depth(); feature_number++) {
 				double partial_derivative = 0;
 				for (int sample_number = 0; sample_number < training_dataset_ -> size(); sample_number++) {
@@ -38,14 +55,13 @@ class LinearRegressor {
 					parameters_[feature_number] -= (learning_rate * partial_derivative);
 				}
 			}
-			cout << "RMS error is: " << rms_error << "\n";
 		}
 	}
 public:
-	void train(Dataset * training_dataset, double learning_rate, double convergence_factor) {
+	void train(Dataset * training_dataset, double learning_rate, double allowed_rms_error) {
 		training_dataset_ = training_dataset;
 		parameters_.resize(training_dataset_ -> depth() + 1, 0.5);
-		gradientDescent(learning_rate, convergence_factor);
+		gradientDescent(learning_rate, allowed_rms_error);
 	}
 
 	void printResults() {
